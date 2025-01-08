959622400e2b33110ca4ed7e056530f5952a4762
Abhinash Shukla <abhinashshukla2002@gmail.com>
2025-01-08 11:14:28 +0530

    divide

--- Class2.java
+++ Class2.java
@@ -8,11 +8,15 @@ public class Class2{
     public static double mul(double a,double b){
         return a*b;
     }
+    public static double div(double a,double b){
+        return a/b;
+    }
     public static void main(String [] args){
         double a=10;
         double b=3;
         System.out.println(add(a, b));
         System.out.println(sub(a, b));
         System.out.println(mul(a, b));
+        System.out.println(div(a, b));
     }
 }

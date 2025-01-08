af0522ce84267b661388ff4aa0f8abfd5170715b
Abhinash Shukla <abhinashshukla2002@gmail.com>
2025-01-08 11:11:01 +0530

    substraction

--- Class2.java
+++ Class2.java
@@ -2,9 +2,13 @@ public class Class2{
     public static double add(double a,double b){
         return a+b;
     }
+    public static double sub(double a,double b){
+        return a-b;
+    }
     public static void main(String [] args){
         double a=10;
         double b=3;
         System.out.println(add(a, b));
+        System.out.println(sub(a, b));
     }
 }

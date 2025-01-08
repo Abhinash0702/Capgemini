8c7af7112617d3a6cc50d08af99f55b5056f5102
Abhinash Shukla <abhinashshukla2002@gmail.com>
2025-01-08 11:09:28 +0530

    addition

--- Class2.java
+++ Class2.java
@@ -1,6 +1,10 @@
 public class Class2{
+    public static double add(double a,double b){
+        return a+b;
+    }
     public static void main(String [] args){
         double a=10;
         double b=3;
+        System.out.println(add(a, b));
     }
 }

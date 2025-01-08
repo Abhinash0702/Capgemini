e03bb9d23b75fa95c0a2f9c4e8864654aea444e7
Abhinash Shukla <abhinashshukla2002@gmail.com>
2025-01-08 11:12:34 +0530

    multi

--- Class2.java
+++ Class2.java
@@ -5,10 +5,14 @@ public class Class2{
     public static double sub(double a,double b){
         return a-b;
     }
+    public static double mul(double a,double b){
+        return a*b;
+    }
     public static void main(String [] args){
         double a=10;
         double b=3;
         System.out.println(add(a, b));
         System.out.println(sub(a, b));
+        System.out.println(mul(a, b));
     }
 }
